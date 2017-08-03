febca482ffa56f41788e693503ca92dc594122d2
ianmitch90 <ianmitch90@gmail.com>
2017-08-03 10:43:25 -0500

    ok got state to werk

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,11 @@
 export const FILTER_PRODUCTS = 'FILTER_PRODUCTS';
 
-export const filterProducts = function (filterType) {
-  return {
-    type: FILTER_PRODUCTS,
-    payload: filterType
-  }
-}
+// export const filterProducts = function (filterType) {
+//   return {
+//     type: FILTER_PRODUCTS,
+//     payload: filterType
+//   }
+// }
 
 const makeActionCreator = function (actionType) {
   return function (payload) {
